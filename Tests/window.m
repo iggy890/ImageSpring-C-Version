c69b6433f0bbb029e717446fcd267ff04580b5e3
Family Account <familyaccount@Onnos-MacBook-Air.local>
2022-07-07 15:13:31 +0100

    Changes to window.m

--- Tests/window.m
+++ Tests/window.m
@@ -21,23 +21,27 @@
 @end
 */
 
-NSColor *createColor(float r, float g, float b) {
-    return [NSColor colorWithCalibratedRed:r green:g blue:b alpha:1.0f];
+NSColor *createColor(float r, float g, float b, float a=0) {
+    return [NSColor colorWithCalibratedRed:r green:g blue:b alpha:a];
+}
+
+NSRect createRect(float startX, float startY, float width, float height) {
+    return NSMakeRect(startX, startY, width, height);
 }
 
 int main() {
 @autoreleasepool {
         // Create a default window
         NSWindow *window = [[NSWindow alloc] init];
-        window.title = @"Test???";
-        window.subtitle = @"Bottom Text";
-        [window setFrame:NSMakeRect(0.f, 0.f, 300.f, 300.f) display:YES animate:YES];
+        window.title = @"Title";
+        window.subtitle = @"Subtitle";
+        [window setFrame:createRect(0, 0, 300, 300) display:YES animate:YES];
         // Set the color
         [window setBackgroundColor: createColor(0.5, 1, 0.1)];
         // Bring to front and make it key
         [window makeKeyAndOrderFront:nil];
         while (1) {
-            NSEvent* event = [window nextEventMatchingMask:NSEventMaskAny]; //window shows now
+            NSEvent *event = [window nextEventMatchingMask:NSEventMaskAny]; //window shows now
         }
     }
 }
