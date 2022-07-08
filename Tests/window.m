5c534c54d6fb9ae65b6facd97d20b322bd068e46
Family Account <familyaccount@Onnos-MacBook-Air.local>
2022-07-08 12:55:28 +0100

    Changes to window.m

--- Tests/window.m
+++ Tests/window.m
@@ -1,27 +1,6 @@
 #include <Cocoa/Cocoa.h>
 
-/*
-@interface window:NSObject {
-    NSWindow *window = [[NSWindow alloc] init];
-    void init() {
-        
-    }
-    
-    void setColor(NSColor color) {
-        [window setBackgroundColor:[NSColor color]];
-    }
-    
-    void size(int width, int height) {
-        
-    }
-    void mainloop() {
-        
-    }
-}
-@end
-*/
-
-NSColor *createColor(float r, float g, float b, float a=0) {
+NSColor *createColor(float r, float g, float b, float a) {
     return [NSColor colorWithCalibratedRed:r green:g blue:b alpha:a];
 }
 
@@ -29,21 +8,18 @@ NSRect createRect(float startX, float startY, float width, float height) {
     return NSMakeRect(startX, startY, width, height);
 }
 
-int main() {
-@autoreleasepool {
-        // Create a default window
+int main() { @autoreleasepool {
         NSWindow *window = [[NSWindow alloc] init];
         window.title = @"Title";
         window.subtitle = @"Subtitle";
 
-        NSRect *pos = createRect(10, 10, 20, 20);
-        NSTextField *yourLabel = [[NSTextField alloc] initWithFrame:pos];
+        NSText *t = [[NSText alloc] initWithFrame: createRect(10, 10, 20, 20)];
+        t.string = @"test";
+        [window.contentView addSubview: t];
 
         [window setFrame:createRect(0, 0, 300, 300) display:YES animate:YES];
-
-        [window setBackgroundColor: createColor(0.5, 1, 0.1)];
+        [window setBackgroundColor: createColor(0.5, 1, 0.1, 1)];
         [window makeKeyAndOrderFront:nil];
-
         while (1) {
             NSEvent *event = [window nextEventMatchingMask:NSEventMaskAny];
         }
