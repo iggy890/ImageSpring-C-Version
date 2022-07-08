20ebb5fe4f5eee2ce78aa10e669c343ff9a7d162
Family Account <familyaccount@Onnos-MacBook-Air.local>
2022-07-08 13:32:02 +0100

    Changes to window

--- Tests/window.m
+++ Tests/window.m
@@ -13,15 +13,15 @@ int main() { @autoreleasepool {
         window.title = @"Title";
         window.subtitle = @"Subtitle";
 
-        NSText *t = [[NSText alloc] initWithFrame: createRect(10, 10, 20, 20)];
-        t.string = @"test";
+        NSText *t = [[NSText alloc] initWithFrame: createRect(50, 100, 100, 10)];
+        t.string = @"What is your name?";
         [window.contentView addSubview: t];
 
-        [window setFrame:createRect(0, 0, 300, 300) display:YES animate:YES];
+        [window setFrame: createRect(0, 0, 200, 200) display:YES animate:YES];
         [window setBackgroundColor: createColor(0.5, 1, 0.1, 1)];
         [window makeKeyAndOrderFront:nil];
         while (1) {
-            NSEvent *event = [window nextEventMatchingMask:NSEventMaskAny];
+            NSEvent *event = [window nextEventMatchingMask: NSEventMaskAny];
         }
     }
 }
