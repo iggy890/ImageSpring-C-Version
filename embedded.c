af414656b21f56e2cee2fca155c4d8a54f6a9bf8
Family Account <familyaccount@Onnos-MacBook-Air.local>
2022-07-09 10:41:43 +0100

    Added new files

---
+++ embedded.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "Headers/Python.h"
+
+int main()
+{
+	PyObject* pInt;
+
+	Py_Initialize();
+
+	PyRun_SimpleString("print('Hello World from Embedded Python!!!')");
+	
+	Py_Finalize();
+
+	printf("\nPress any key to exit...\n");
+	if(!_getch()) _getch();
+	return 0;
+}
